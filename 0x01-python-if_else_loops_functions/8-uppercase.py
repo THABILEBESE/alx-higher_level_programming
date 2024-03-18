fabd7ce441ccb198b277961dd896e10c0312fbc2
THABILEBESE <thabilebese@icloud.com>
2024-03-18 04:15:42 -0700

    update

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-import sys
 
 def uppercase(s):
-    for iterator in s:
-        temp = iterator
-        if ord(temp) >= 97 and ord(temp) <= 122:
-            temp = chr(ord(iterator) - 32)
-        sys.stdout.write("{}".format(temp))
+    for char in s:
+        if 'a' <= char <= 'z':
+            char = chr(ord(char) - 32)
+        print(char, end='')
     print()
